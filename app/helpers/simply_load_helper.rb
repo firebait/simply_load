734bb38299a40abc4d3b80c32802d5bd2623ced3
Firebait Project <firebait@localhost.(none)>
2011-07-12 17:27:02 -0500

    simply_load initial release

--- app/helpers/simply_load_helper.rb
+++ app/helpers/simply_load_helper.rb
@@ -1,7 +1,7 @@
-Module SimpleLoadHelper
+Module SimplyLoadHelper
 
   def simply_load(url, content, options={})
-    options[:class] = "#{options[:class]} content_auto_load".strip
+    options[:class] = "#{options[:class]} simply_load".strip
     options.merge!(:"data-href" => url)
     content_tag :div, content, options
   end
--- lib/generators/simply_load/simply_load_generator.rb
+++ lib/generators/simply_load/simply_load_generator.rb
@@ -1,8 +1,8 @@
-class SimplyLoadGenerator < Rails::Generators::NamedBase
+class SimplyLoadGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
       
   def generate_javascript
-    copy_file "simply_load.js", (Rails::VERSION::STRING.starts_with? "3.1" ? "assets/javascripts/simply_load.js" : "public/javascripts/simply_load.js")
+    copy_file "simply_load.js", (Rails::VERSION::STRING.starts_with?("3.1") ? "app/assets/javascripts/simply_load.js" : "public/javascripts/simply_load.js")
   end
       
 end
