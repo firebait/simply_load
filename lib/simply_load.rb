9d7b5e415b8c9af216393ab836e150bdf5cca444
Firebait Project <firebait@localhost.(none)>
2011-07-13 21:57:17 -0500

    changed gem from engine to just include to Action::Base

--- lib/simply_load.rb
+++ lib/simply_load.rb
@@ -2,7 +2,17 @@ require "simply_load/version"
 
 module SimplyLoad
   
-  class SimplyLoad < Rails::Engine
-  end  
+  def simply_load(url, content_or_options_with_block = nil, options = {}, escape = true, &block)
+    options = content_or_options_with_block if content_or_options_with_block.is_a?(Hash) and block_given?
+    options[:class] = "#{options[:class]} simply_load".strip
+    options.merge!(:"data-href" => url)
+    if block_given?
+      content_tag :div, capture(&block), options, escape
+    else
+      content_tag :div, content_or_options_with_block, options, escape
+    end
+  end    
 
 end
+
+ActionView::Base.send(:include, SimplyLoad)
--- lib/simply_load/version.rb
+++ lib/simply_load/version.rb
@@ -1,3 +1,3 @@
 module SimplyLoad
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
