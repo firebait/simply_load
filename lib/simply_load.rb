80b9600169dd2507b9391b6063539a310c52da7c
firebait <firebait@ubuntu.ubuntu-domain>
2011-09-09 10:36:20 -0500

    support for periodically reloading a simply_load element

--- lib/simply_load.rb
+++ lib/simply_load.rb
@@ -11,7 +11,24 @@ module SimplyLoad
     else
       content_tag :div, content_or_options_with_block, options, escape
     end
-  end    
+  end
+
+  def simply_load_timed(t, unit, url, content_or_options_with_block = nil, options = {}, escape = true, &block)
+    timer = t.send(unit).seconds * 1000
+    options = content_or_options_with_block if content_or_options_with_block.is_a?(Hash) and block_given?
+    options[:class] = "#{options[:class]} simply_load simply_load_timed".strip
+    options.merge!(:"data-href" => url, :"data-timer" => timer)
+    if block_given?
+      content_tag :div, capture(&block), options, escape
+    else
+      content_tag :div, content_or_options_with_block, options, escape
+    end
+  end
+
+  def method_missing(method, *args, &block)
+    return simply_load_timed($1.to_i, $2.to_s, *args) if method =~ /^simply_load_every_+(.*\d)+_+(second|seconds|minute|minutes|hour|hours)$/  # Call has_role? with is_role_name?
+    super
+  end  
 
 end
 
